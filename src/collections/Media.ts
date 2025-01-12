7cdc553c218f11de808e0f509b4fc7422be90a05
Terry Seidler <terry.seidler@ogd.nl>
2025-01-12 22:32:12 +0100

    BREAK select after delete

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -11,6 +11,33 @@ export const Media: CollectionConfig = {
       type: 'text',
       required: true,
     },
+    {
+      name: 'cakes',
+      type: 'select',
+      hasMany: true,
+      admin: {
+        position: 'sidebar',
+      },
+      defaultValue: ['1'],
+      options: [
+        {
+          label: '1 cake',
+          value: '1',
+        },
+        {
+          label: '2 cakes',
+          value: '2',
+        },
+      ],
+    },
   ],
+  versions: {
+    drafts: {
+      autosave: {
+        interval: 100, // We set this interval for optimal live preview
+      },
+    },
+    maxPerDoc: 50,
+  },
   upload: true,
 }
